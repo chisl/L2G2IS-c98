6dff2d171472f9fe93855e0598a704500a385085
chisl <info@chisl.io>
2018-01-22 16:43:46 +0100

    Initial commit

---
+++ L2G2IS.h
@@ -0,0 +1,15 @@
+/*
+ * name:        L2G2IS
+ * description: MEMS motion sensor: Ultra-compact two-axis gyroscope for optical image stabilization
+ * manuf:       STMicroelectronics
+ * version:     Version 0.1
+ * url:         http://www.st.com/resource/en/datasheet/l2g2is.pdf
+ * date:        2018-01-22
+ */
+
+/*                                                                                                       *
+ *                                   THIS FILE IS AUTOMATICALLY CREATED                                  *
+ *                                    D O     N O T     M O D I F Y  !                                   *
+ *                                                                                                       */
+
+
